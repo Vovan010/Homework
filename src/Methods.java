c39a95069e12f22e1c1fe9c5598ded4a3a53be5a
Vovan010 <vovashyriaev100@gmail.com>
2024-08-05 17:27:39 +0300

    New commit

---
+++ src/Methods.java
@@ -0,0 +1,9 @@
+public class Methods
+{
+    public void printThreeWords()
+    {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
