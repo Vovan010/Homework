ee52deef87dbdc0994853b71ba64b0ed50d73b30
Vovan010 <vovashyriaev100@gmail.com>
2024-08-19 21:18:11 +0300

    New Commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee();
         employee.someInfo();
+        Employees[] employeesArray = new Employees[5];
     }
 }
