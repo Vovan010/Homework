88b6c49bdfc33330e04cd7ebe40ed5f33e5db8f7
Vovan010 <vovashyriaev100@gmail.com>
2024-08-27 22:11:09 +0300

    1

--- src/MyArrayDataException.java
+++ src/MyArrayDataException.java
@@ -1,5 +1,5 @@
 public class MyArrayDataException extends RuntimeException {
-    public MyArrayDataException( int row, int col){
-        super(String.format("Анализировать исключение int в массиве [ id, id]", row, col));
+    public MyArrayDataException( int a, int b){
+        super(String.format("Анализировать исключение int в массиве [ , ]", a, b));
     }
 }
