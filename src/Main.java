9804f530470500f0497b3e31d536c7381c14371b
Vovan010 <vovashyriaev100@gmail.com>
2024-09-08 23:20:28 +0300

    New Commit

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,19 @@
+//Задание 2
 public class Main {
     public static void main(String[] args) {
-        Animal animal = new Animal();
-        Animal animal1 = new Animal();
-        Animal animal2 = new Animal();
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Иванов", "123-456");
+        phoneBook.add("Иванов", "789-012");
+        phoneBook.add("Петров", "345-678");
+        phoneBook.add("Сидоров", "456-789");
+        phoneBook.add("Петров", "987-654");
 
-        Cat snezok = new Cat("Snezok");
-        Dog sharik = new Dog("Sharik");
-        Cat snezok1 = new Cat("Snezok");
-        Dog sharik1 = new Dog("Sharik");
+        // Получаем номера телефонов по фамилии
+        System.out.println("Телефоны Иванова: " + phoneBook.get("Иванов"));
+        System.out.println("Телефоны Петрова: " + phoneBook.get("Петров"));
 
-        snezok.animalRun(205);
-        snezok.animalRun(125);
-
-        sharik.animalRun(250);
-        sharik.animalRun(505);
-
-        snezok.animalSwim(123);
-
-        sharik.animalSwim(15);
-        sharik.animalSwim(7);
-
-        System.out.println("Всего животных: " + Animal.getCountAnimal());
-        System.out.println("Всего котов: " + Cat.getCountCat());
-        System.out.println("Всего собак: " + Dog.getCountDog());
-
-        int foodInBowl = 15;
-
-        Cat[] cats = {new Cat("Barsik"), new Cat("Pushistic"), new Cat("Belyash")};
-        for (Cat c : cats) {
-            c.eat(5, foodInBowl);
-            foodInBowl -= 5;
-        }
-        for (Cat c : cats) {
-            System.out.println("Кот сыт: " + c.isFed());
-        }
-        addFoodToBowl(5, foodInBowl);
-        System.out.println("Добавлено 5 ед. еды в миску. Всего: " + foodInBowl);
-    }
-
-    private static void addFoodToBowl(int amount, int currentFood) {
-        currentFood += amount;
-        System.out.println("Добавлено " + amount + " единиц еды в миску");
+        // Выводим весь справочник
+        System.out.println("\nВесь телефонный справочник:");
+        phoneBook.printAll();
     }
 }
